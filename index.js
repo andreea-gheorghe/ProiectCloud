b62909d4af32d4114dd11c2f8447a3d53b1be1ff
andreea-gheorghe <gheorgheandreea18@stud.ase.ro>
2022-05-14 16:59:33 +0300

    First routes and a router

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const cors=require('cors');
+const bodyParser=require("body-parser");
+
+const messagesRouter=require('./messagesRouter');
 
 const app=express();
 app.use(cors());
 
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({extened:true}));
+
+app.use("/messages",messagesRouter);
+
 const port= process.env.PORT||8080;
 
-app.get('/', (req, res)=>{
-    res.send('Hello Wrorld!')
-});
 
 app.listen(port, ()=>{
     console.log(`Cloud computing app listening on port ${port}!`)
